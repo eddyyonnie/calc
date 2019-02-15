25827329e72c717a7336a1d9d6ec90b52d63c969
eddy <eddyyonnie@gmail.com>
2019-02-15 11:23:38 +0300

    changes included such as parseInt

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var add=function(number1,number2){
+return number1 + number2;
+};
+var ad=(add(50,50));
+var number1=parseInt(prompt("write a number"));
+var number2=parseInt(prompt("write another number"));
+var yes=(add(number1,number2))
+alert(yes);
