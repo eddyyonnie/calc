d0f28c4aeadac5541d1483098d8276a8887c4f25
eddy <eddyyonnie@gmail.com>
2019-02-15 11:56:35 +0300

    made gh-pages and master similar

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
+
 var add=function(number1,number2){
 return number1 + number2;
 };
 var ad=(add(50,50));
+alert(ad)
+
 var number1=parseInt(prompt("write a number"));
 var number2=parseInt(prompt("write another number"));
 var yes=(add(number1,number2))
