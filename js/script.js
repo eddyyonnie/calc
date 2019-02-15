455d7d58bb59f98dccdb12d1f98708f0269c6ce9
eddy <eddyyonnie@gmail.com>
2019-02-15 12:05:31 +0300

    included necessary changes

--- js/script.js
+++ js/script.js
@@ -1,50 +1,10 @@
-var add = function(number1,number2){
-  return number1 + number2;
-};
-var number1=parseInt(prompt("Enter a number:"));
-var number2=perseInt(prompt("Enter another number:"));
-var results=add(number1,number2);
-alert(results);
-
-function add(firstNumber,secondNumber){
-return firstNumber + secondNumber;
-}
-var five = 5;
-var six = 6;
-var ad = add (five,six);
-
-alert(ad);
-var multiply=function(first,second){
-return first* second;
-}
-var one=23;
-var two=24;
-var malt=multiply(one,two);
-alert(malt);
-
-var name=function(yonnie){
-	alert("hey there " + yonnie)
-}
-var add = function (number1,number2){
+(number1,number2){
 return number1 + number2;
 };
-var ad=add(2,5);
-alert(ad);
-var subtract =function(number1,number2){
-return number1-number2;
-}
-var dojo=subtract(20,1);
-alert(dojo);
-var add=function(number1,number2){
-return number1+number2;
-};
-var number1=prompt("enter a number:");
-var number2=prompt("enter another number:");
-alert(add(number1,number2));
+var ad=(add(50,50));
+alert(ad)
 
-name(prompt("write your name"));
-var add=function(number1,number2){
-return number1 + number2;
-}
-var ad=add(20,42);
-alert(ad);
+var number1=parseInt(prompt("write a number"));
+var number2=parseInt(prompt("write another number"));
+var yes=(add(number1,number2))
+alert(yes);
